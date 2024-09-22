af348aa14877ecf7adc780905296c6b359dfd188
vincent yap <lastency@gmail.com>
2024-09-22 20:58:18 +0800

    initial commit

---
+++ Model/FibonacciContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FibonacciTest.Model
+{
+    public class FibonacciContext : DbContext
+    {
+        public DbSet<RunSummary> RunSummary { get; set; }
+        public DbSet<RunOutput> RunOutput { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(@$"Server=your_server_name;Database=Fibonacci;Encrypt=false;Integrated Security=True;");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RunSummary>().ToTable("run_summary").HasKey(rs => rs.RunId);
+            modelBuilder.Entity<RunOutput>().ToTable("run_output").HasKey(ro => ro.Id); 
+
+            modelBuilder.Entity<RunOutput>()
+           .HasOne(ro => ro.RunSummary)
+           .WithOne(rs => rs.RunOutput)
+           .HasForeignKey<RunSummary>(ro => ro.RunId);
+        }
+    }
+
+    public class RunSummary
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("run_id")]
+        public int RunId { get; set; }
+
+        [Column("run_datetime")]
+        public DateTime RunDateTime { get; set; }
+        public string UserName { get; set; }
+
+        [Column("sequence_count")]
+        public int SequenceCount { get; set; }
+
+        public RunOutput RunOutput { get; set; }
+
+    }
+
+    public class RunOutput
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("RunSummary")]
+        [Column("run_id")]
+        public int RunId { get; set; }
+        public string Output { get; set; }
+        public RunSummary RunSummary { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using FibonacciTest.Model;
+using System.Text;
+
+namespace FibonacciTest
+{
+    internal class Program
+    {
+        private string _userName = "";
+        private int _sequenceCount;
+        private string _output = "";
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.DisplayRunPrompt();
+            program.GenerateFibonacciSequence();
+            program.SaveResult();
+
+        }
+
+        private void DisplayRunPrompt()
+        {
+            Console.WriteLine("Welcome to the Fibonacci Generator!");
+            _userName = PromptForUsername();
+
+            _sequenceCount = PromptForSequenceCount();
+        }
+
+        private string PromptForUsername()
+        {
+            string input;
+            do
+            {
+                Console.Write("Please enter your username:");
+                input = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(input));
+
+            return input;
+        }
+
+        private int PromptForSequenceCount()
+        {
+            string input;
+            int sequenceCount;
+            do
+            {
+                Console.Write("Please enter the number of Fibonacci sequence numbers to generate:");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out sequenceCount) || sequenceCount <= 0);
+
+            return sequenceCount;
+        }
+
+        private void GenerateFibonacciSequence()
+        {
+                Console.WriteLine($"Generating {_sequenceCount} Fibonacci numbers for {_userName}:");
+
+                int first = 0, second = 1, next;
+                StringBuilder outputSb = new StringBuilder();
+
+                for (int i = 0; i < _sequenceCount; i++)
+                {
+                    if (i <= 1)
+                        next = i;
+                    else
+                    {
+                        next = first + second;
+                        first = second;
+                        second = next;
+                    }
+                    outputSb.Append($"{next}, ");
+                }
+                _output = outputSb.ToString().TrimEnd(' ',','); 
+        }
+
+        private void SaveResult()
+        {
+            using (var dbContext = new FibonacciContext())
+            {
+                var runSummary = new RunSummary() { SequenceCount = _sequenceCount, UserName = _userName, RunDateTime = DateTime.Now };
+                dbContext.RunSummary.Add(runSummary);
+                dbContext.SaveChanges();
+
+                dbContext.RunOutput.Add(new RunOutput() { RunId = runSummary.RunId, Output = _output });
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
